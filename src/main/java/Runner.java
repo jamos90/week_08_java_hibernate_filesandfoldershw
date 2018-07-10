23db3baec2ba45a9fabfe02f2b4b84c33926441b
jamos90 <jamos_98@hotmail.com>
2018-07-10 16:29:56 +0100

    save method working

---
+++ src/main/java/Runner.java
@@ -0,0 +1,18 @@
+import db.DBHelper;
+import models.File;
+import models.Folder;
+
+public class Runner {
+
+    public static void main(String[] args){
+        Folder photos = new Folder("Holiday Photos");
+        DBHelper.save(photos);
+
+        File file1 = new File("Photo_1",".jpg",33, photos);
+        DBHelper.save(file1);
+
+        File file2 = new File("Photo_2",".png",40,photos);
+        DBHelper.save(file2);
+
+    }
+}
